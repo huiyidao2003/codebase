065463c923e47f81c164593aa328564d029c29a1
zhengcunwen <zhengcunwen@waqu.com>
2016-08-18 14:13:25 +0800

    加入list转字符串代码

--- javacase/src/main/java/org/victor/casejava/commons/StringTest.java
+++ javacase/src/main/java/org/victor/casejava/commons/StringTest.java
@@ -1,8 +1,25 @@
 package org.victor.casejava.commons;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import java.util.List;
+
 /**
  * apache commons包的字符串增强处理
  * Created by zhengcunwen on 2016/8/18.
  */
+
 public class StringTest {
+
+
+    /**list或者array转字符串，特殊符号分割**/
+    @Test
+    public void listToString(){
+        List<Integer> list = Lists.newArrayList(1,3,5,2,223,12);
+        String to = StringUtils.join(list, ",");
+
+        System.out.println(to);
+    }
 }
